1c1214811c7c7cd67ae0ff6b6910e75d16d71771
Vishnu Shabu <vishnukalavappilly@outlook.com>
2022-07-07 08:31:36 +0530

    Battle ship game - initial commit - business logic to get the count of sunk ships

--- Battleships.Test/GameTest.cs
+++ Battleships.Test/GameTest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Battleships;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Xunit;
 
 namespace Battleships.Test
@@ -12,10 +7,10 @@ namespace Battleships.Test
     {
         [Fact]
         public void TestPlay()
-        {
-            var ships = new[] { "3:2,3:5" };
-            var guesses = new[] { "7:0", "3:3" };
-            Game.Play(ships, guesses).Should().Be(0);
+       {
+            var ships = new[] { "3:2,3:5", "1:2,2:2", "4:3,4:5", "5:0,7:0", "0:8,3:8",  };
+            var guesses = new[] { "7:0", "3:2", "3:3", "3:4", "3:5", "4:4","7:7", "1:2","2:2","4:3","4:4","4:5","2:8","3:8" };
+            Game.Play(ships, guesses).Should().Be(3);
         }
     }
 }
--- Battleships/Game.cs
+++ Battleships/Game.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Battleships
 {
@@ -18,9 +16,73 @@ namespace Battleships
         // guesses: each string represents the co-ordinate of a guess
         //   e.g. "7:0" - misses the ship above, "3:3" hits it.
         // returns: the number of ships sunk by the set of guesses
+
+        private static readonly List<List<string>> _ships = new List<List<string>>();
+
+        /// <summary>
+        /// The battle ship game
+        /// </summary>
+        /// <param name="ships">Coordinates of ships passed as a collection (eg: ["3:2,3:5", "4:2,4:5"])</param>
+        /// <param name="guesses">Collection of coordinates of ship to be sunk eg: ["3:4","7:3"]</param>
+        /// <returns>Count of sunk ships</returns>
         public static int Play(string[] ships, string[] guesses)
         {
-            return 0;
+            SetShipCellCollection(ships);
+            return GetSunkShipCount(guesses);
         }
+
+        /// <summary>
+        /// Sets the ships collection 
+        /// </summary>
+        /// <param name="ships">Coordinates of ships passed as a collection (eg: ["3:2,3:5", "4:2,4:5"])</param>
+        /// <exception cref="InvalidOperationException"></exception>
+        private static void SetShipCellCollection(string[] ships)
+        {
+            foreach (var ship in ships)
+            {
+                var coordinates = ship.Split(',');
+                var firstCoordinates = coordinates[0].Split(':');
+                var secondCoordinates = coordinates[1].Split(':');
+                var tempShip = new List<string>();
+                if (firstCoordinates[0] == secondCoordinates[0]) //horizontal ship
+                {
+                    var shipHead = Convert.ToInt32(firstCoordinates[1]);
+                    var shipFoot = Convert.ToInt32(secondCoordinates[1]);
+                    var currentShipPosition = firstCoordinates[0];
+                    while (shipHead <= shipFoot)
+                    {
+                        tempShip.Add($"{currentShipPosition}:{shipHead}");
+                        shipHead++;
+                    }
+                }
+                else if (firstCoordinates[1] == secondCoordinates[1]) //vertical ship
+                {
+                    var shipHead = Convert.ToInt32(firstCoordinates[0]);
+                    var shipFoot = Convert.ToInt32(secondCoordinates[0]);
+                    var currentShipPosition = firstCoordinates[1];
+                    while (shipHead <= shipFoot)
+                    {
+                        tempShip.Add($"{shipHead}:{currentShipPosition}");
+                        shipHead++;
+                    }
+                }
+                else
+                {
+                    throw new InvalidOperationException("No diagonal ships are allowed");
+                }
+                if (tempShip.Any())
+                {
+                    _ships.Add(tempShip);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the sunk ship count based on the player guesses
+        /// </summary>
+        /// <param name="guesses">Collection of coordinates of ship to be sunk eg: ["3:4","7:3"]</param>
+        /// <returns>The number of ships sunk</returns>
+        private static int GetSunkShipCount(string[] guesses) =>
+            _ships.Count(ship => ship.All(shipCell => guesses.Contains(shipCell)));
     }
 }
